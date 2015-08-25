587f6311784eb831706fd7dcebede8ee2211738d
michael bunker & Clinton Herrick <student@epicodus.com>
2015-08-25 13:41:40 -0700

    add route for add_survey button

--- app.rb
+++ app.rb
@@ -10,11 +10,20 @@ get '/' do
   erb :index
 end
 
-
 get '/survey/new' do
   erb :survey_add
 end
-
+#
+# get '/survey/:id' do
+#   survey_id = params.fetch('id').to_i
+#   @survey = Survey.find(survey_id)
+#   @questions = @survey.questions
+#   erb :survey_detail
+# end
+get '/survey' do
+  @surveys = Survey.all
+  erb :index
+end
 
 post '/survey' do
   name = params.fetch('survey_name')
