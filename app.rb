7ac7b84dbfcaed9865a6f725679a960a6f0e6dd4
michael bunker & Clinton Herrick <student@epicodus.com>
2015-08-25 12:26:28 -0700

    add index page and survey add form

--- app.rb
+++ app.rb
@@ -9,3 +9,16 @@ get '/' do
   @surveys = Survey.all
   erb :index
 end
+
+
+get '/survey/new' do
+  erb :survey_add
+end
+
+
+post '/survey' do
+  name = params.fetch('survey_name')
+  @survey = Survey.create({:name => name})
+  @surveys = Survey.all
+  erb :index
+end
